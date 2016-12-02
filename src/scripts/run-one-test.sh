883c17efba5846edb3f87a4ad37b7b62bcfb06bd
Böszörményi Zoltán <zboszormenyi@sicom.com>
2016-12-02 08:36:19 +0100

    RLIB-23: Add documentation to src/scripts/run-one-test.sh

--- src/scripts/run-one-test.sh
+++ src/scripts/run-one-test.sh
@@ -1,7 +1,52 @@
 #!/bin/bash
 #
-# This script is for running regression tests
-# It expects a file called something.test when it is called
+# This script is for running one regression test.
+#
+# $1 is the directory where it should be running from
+# $2 is the test name
+#
+# It also expects a file called testname.test when it is called
+# which is a shell script file with the environment variables
+# described below.
+#
+# COMMAND="command"
+#   The command to run for the test.
+#	The command must accept the output format in text
+#	as the last argument. See "FORMATS" below.
+# EXTRA_COMMAND="extra_command"
+#	Extra command to run after the test has run
+#	and the result is compared with the expected output(s).
+#	Can be used to clean up supplementary output files,
+#	like PNGs created for the HTML output.
+# FORMATS="pdf xml txt csv html"
+#	All the formats that the test supports.
+#	Some tests may only need "txt" when certain
+#	error cases are tested. It is the "testname.txt.stderr"
+#	that's interested in this case.
+#	Only "pdf", "xml", "txt", "csv" and "html" are accepted
+#	in the FORMATS variable as a space separated list.
+#	Any other word is still passed in as-is to the test
+#	but it may fail and it is not handled in the settings below.
+# EXPECTED_pdf=
+# EXPECTED_xml=
+# EXPECTED_txt=
+# EXPECTED_csv=
+# EXPECTED_html=
+#	When there are multiple accepted outcomes for a specific
+#	output format, these are listed in these variables. E.g. "1 2".
+#	These values are appended to the expected output name
+#	to compare with. These files may exist in the "expected"
+#	directory:
+#
+#	testname.format.stderr1
+#	testname.format.stderr2
+#	testname.format.stdout1
+#	testname.format.stdout2
+#
+#	These will be compared to the current result and if
+#	one of them matches, the result is accepted.
+#	If one of this variable
+#
 
 DIR="$1"
 FILE="$2"
