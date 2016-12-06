4c5ef34eb661f477c31601f9fe4cfdc190fe0ad3
Böszörményi Zoltán <zboszormenyi@sicom.com>
2016-12-06 18:39:20 +0100

    RLIB-23: Added a different use case for csv.php
    Now that the use of the header() function works, let's distinguish
    between the old and new uses.

--- src/php/csv.php
+++ src/php/csv.php
@@ -1,7 +1,14 @@
 <?php 
+	if (isset($argv[1])) {
+		if (strcmp($argv[1], "--newheaders") == 0)
+			$idx = 2;
+		else
+			$idx = 1;
+	}
+
 	$output_format = 'txt';
-	if (isset($argv[1]))
-		$output_format = $argv[1];
+	if (isset($argv[$idx]))
+		$output_format = $argv[$idx];
 
 	$rlib =	rlib_init();
 	rlib_set_output_format_from_text($rlib, $output_format);
@@ -10,7 +17,18 @@
 	rlib_add_report($rlib, "csv.xml");
 	rlib_set_output_parameter($rlib, "debugging", "yes");
 	rlib_execute($rlib);
-	header(rlib_get_content_type($rlib));
+
+	if ($idx = 2) {
+		$content_type = rlib_get_content_type($rlib);
+		$replace_header = true;
+		while (strlen($content_type) > 0) {
+			header($content_type, $replace_header);
+			$content_type = rlib_get_content_type($rlib);
+			$replace_header = false;
+		}
+	} else {
+		header(rlib_get_content_type($rlib));
+	}
 	rlib_spool($rlib);
 	rlib_free($rlib);	
 ?>
