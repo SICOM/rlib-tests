604a67940155635d540c78db23227faadd4c0efa
Böszörményi Zoltán <zboszormenyi@sicom.com>
2016-12-06 13:14:44 +0100

    RLIB-23: Added expected data for src/php/array.php
    Modified src/php/array.php to accept the output format in a common way.

--- src/php/array.php
+++ src/php/array.php
@@ -11,14 +11,18 @@
 	$data[1][3] = "1";
 	$data[1][4] = "Green Eggs And Spam I Am I Am";
 
+	$output_format = 'txt';
+	if (isset($argv[1]))
+		$output_format = $argv[1];
+
 	$rlib =	rlib_init();
 	rlib_version();
+	rlib_set_output_format_from_text($rlib, $output_format);
 	rlib_add_datasource_array($rlib, "local_array");
 	rlib_add_query_as($rlib, "local_array", "data", "data");
 	rlib_add_report($rlib, "array.xml");
 	rlib_add_function($rlib, "bobdoan", "bobdoan", 1);
 	rlib_add_function($rlib, "mikeroth", "mike_roth", 2);
-	rlib_set_output_format_from_text($rlib, "json");
 	rlib_set_output_parameter($rlib, "debugging", "yes");
 	rlib_set_output_parameter($rlib, "only_quote_strings", "yes");
 	rlib_set_locale($rlib, "en_US");
