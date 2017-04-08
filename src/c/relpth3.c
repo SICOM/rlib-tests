a4ecc622259c327d97edd81144a8b32a8eb54486
Böszörményi Zoltán <zboszormenyi@sicom.com>
2017-04-08 10:39:48 +0200

    RLIB-23: New regression test relpth3
    It is a copy of relpth2 with PDF compression turned on.

---
+++ src/c/relpth3.c
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (C) 2016 SICOM Systems, INC.
+ *
+ *  Author: Zoltán Böszörményi <zboszormenyi@sicom.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <rlib.h>
+
+int main(int argc, char **argv) {
+	rlib *r;
+
+	if (argc == 1) {
+		printf("usage: %s [ pdf | xml | txt | csv | html ]\n", argv[0]);
+		return 1;
+	}
+
+	r = rlib_init();
+	rlib_set_output_parameter(r, "html_image_directory", "/tmp");
+	rlib_set_output_parameter(r, "compress", "on");
+	rlib_add_report(r, "relpath/relpath-png.xml");
+	rlib_set_output_format_from_text(r, argv[1]);
+	rlib_execute(r);
+	rlib_spool(r);
+	rlib_free(r);
+	return 0;
+}
